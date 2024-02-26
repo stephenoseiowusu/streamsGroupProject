aa39aa137773337f283f625edfbe29a96495a2e7
stephenosei-owusuwork <stephenoseiowusu.jobs@gmail.com>
2024-02-26 13:32:54 -0500

    2/26/24

--- src/main/java/challenges/StudentOps.java
+++ src/main/java/challenges/StudentOps.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import data.FetchData;
 import domain.Student;
 
@@ -8,15 +9,19 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StudentOps {
     //  1. Filter Students by Gender: Write a method to filter a list of students by their gender.
 
     public static List<Student> filterStudentsByGender(List<Student> students, String gender) {
         return students.stream()
-                .filter(student -> student.getGender().equalsIgnoreCase(gender))
-                .toList();
+                .filter(student -> student.getGender().equalsIgnoreCase(gender)).collect(Collectors.toList());
     }
 
     //2. Sort Students by Age: Sort the list of students by their age (based on date of birth).
@@ -29,7 +34,111 @@ public class StudentOps {
         });
         return  students.stream()
                     .sorted(Comparator.comparing(Student::getAge))
-                    .toList();
+                    .collect(Collectors.toList());
         }
 
+    public static int getAverageAgeOfStudents(List<Student> students){
+        return (int)Math.floor(students.stream().flatMapToInt(s ->
+             IntStream.of(s.getAge())
+        ).average().getAsDouble());
+    }
+
+    public static void printAllStudentNames(List<Student> students){
+        students.stream().forEach((student) ->{
+            System.out.printf("%s  %s \n",student.getFirst_name(),student.getLast_name());
+        });
+    }
+
+    public static Map<String,List<Student>> groupStudentsByGender(List<Student> students){
+        return students.stream().collect(Collectors.groupingBy(student -> student.getGender()));
+    }
+
+    public static int getMaxAge(List<Student> students){
+        return students.stream().max((s1, s2) ->{
+            return s1.getAge() - s2.getAge();
+        }).get().getAge();
+    }
+
+    public static Map<Integer,Student> transformListToMap(List<Student> students){
+         return students.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
+    }
+
+    public static List<String> getStudentsEmails(List<Student> students){
+        return students.stream().map((s) ->{
+            return s.getEmail();
+        }).collect(Collectors.toList());
+    }
+
+    public static boolean checkIfStudentISAdult(Student student){
+        return student.getAge() >= 18;
+    }
+
+    public static Map<String, Long> getStudentsCountByGender(List<Student> students){
+        return students.stream().collect(Collectors.groupingBy(Student::getGender,Collectors.counting()));
+    }
+
+    public Student getYoungestFemaieStudent(List<Student> students){
+        return students.stream().filter((s) ->{
+            return s.getGender().equalsIgnoreCase("F");
+        }).min((s1,s2) ->{
+            return s1.getAge() - s2.getAge();
+        }).get();
+    }
+
+    public String getAllStudentFirstNames(List<Student> students){
+        return students.stream().flatMap(s -> Stream.of(s.getFirst_name() + " " + s.getLast_name())).collect(Collectors.joining(", "));
+    }
+
+    public int getSumOfallStudentAges(List<Student> students)
+    {
+        return students.stream().flatMapToInt((student) ->{
+            return IntStream.of(student.getAge());
+        }).sum();
+    }
+
+    public Boolean checkIfAllStudentsAreAdults(List<Student> students){
+        AtomicBoolean result = new AtomicBoolean(true);
+        students.stream().forEach((student) ->{
+            if(student.getAge() < 18){
+                result.set(false);
+            }
+        });
+        return result.get();
+    }
+
+    public Student getOldestStudent(List<Student> students){
+        return students.stream().max((s1,s2) ->{
+            return s1.getAge() - s2.getAge();
+        }).get();
+    }
+
+    public List<Student> converAllStudentFirstNamesToUpperCase(List<Student> students){
+        return students.stream().map((student) ->{
+            return new Student(student.getId(),student.getFirst_name().toUpperCase(),student.getLast_name(),student.getEmail(),student.getGender(),student.getDob().toString());
+        }).collect(Collectors.toList());
+    }
+
+    public Student getStudentById(List<Student> students,int id){
+        return students.stream().filter(s ->{
+            return s.getId() == id;
+        }).findFirst().orElse(null);
+    }
+
+    public Map<Integer,List<Student>> getStudentsByAge(List<Student> students){
+        return students.stream().collect(Collectors.groupingBy(Student::getAge));
+    }
+
+    public double getStandardDeviationOfStudentsAge(List<Student> students){
+        int age_sum = students.stream().mapToInt((s) ->{
+            return s.getAge();
+        }).sum();
+        double age_avg = students.stream().mapToInt((s) ->{
+            return s.getAge();
+        }).average().getAsDouble();
+        AtomicDouble standard_deviation = new AtomicDouble(0.0);
+        students.stream().forEach((s) ->{
+            standard_deviation.addAndGet( Math.pow(s.getAge() - age_avg,2));
+        });
+        return Math.sqrt(standard_deviation.get()/students.size());
+    }
 }
